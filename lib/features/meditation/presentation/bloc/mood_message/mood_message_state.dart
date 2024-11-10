08447ec76041efe3b6ab9b663d96c6d974e03220
keple_bengkel <rgtgilang@gmsil.com>
2024-11-11 00:13:23 +0700

    first commit

---
+++ lib/features/meditation/presentation/bloc/mood_message/mood_message_state.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: unused_import
+
+import 'package:mental_health/features/meditation/domain/entities/daily_quote.dart';
+import 'package:mental_health/features/meditation/domain/entities/mood_message.dart';
+
+abstract class MoodMessageState {}
+
+class MoodMessageInitial extends MoodMessageState{}
+
+class MoodMessageLoading extends MoodMessageState{}
+
+class MoodMessageLoaded extends MoodMessageState{
+  final MoodMessage moodMessage;
+
+  MoodMessageLoaded({required this.moodMessage});
+}
+
+class MoodMessageError extends MoodMessageState{
+  final String message;
+
+  MoodMessageError({required this.message});
+}
